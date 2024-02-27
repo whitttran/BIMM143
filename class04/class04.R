d4441dce1a457a841a590dac8e536ad5a2961d1a
Whitney Tran <wttran@ucsd.edu>
2024-02-27 12:33:10 -0800

    add class04

---
+++ class04/class04.R
@@ -0,0 +1,6 @@
+x<-1:50
+plot(x, sin(x))
+plot(x,sin(x), typ="l")
+plot(x, sin(x), typ="l", col="blue")
+plot(x, sin(x), typ="l", col="blue", lwd=2)
+plot(x, sin(x), typ="l", col="blue", lwd=2, xlab="Hello")
